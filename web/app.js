45b85e85139cea8b4ae2d71218a0ae64eb1c3131
musnows <ezplayingd@126.com>
2026-08-04 14:49:46 +0800

    fix(web): align model testing metadata

--- web/app.js
+++ web/app.js
@@ -459,10 +459,6 @@ function formatTestedAt(value) {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
   }).format(date).replaceAll("/", "-");
 }
 
